c3188c80f19bd385ee093cd0aa259b42420aed09
arbenitaasllani <94245895+arbenitaasllani@users.noreply.github.com>
2022-12-31 13:35:43 +0100

    Add files via upload

---
+++ util/places.dart
@@ -0,0 +1,75 @@
+List places = [
+  {
+    "name": "Tokyo Bay Shiomi Prince Hotel",
+    "img": "assets/6.jpg",
+    "price": r"$136/night",
+    "location": "Tokyo, Japan",
+    "details":  "Tokyo Bay Shiomi Price Hotel features accommodation with a restaurant. "
+        "This 4-star hotel offers a 24-hour front desk. "
+        "Guest at our Hotel can enjoy a buffet breakfast. "
+        "\nAt the accommodation guests are welcome to take advantage of a sauna. "
+        "Popular points of interest near Tokyo Bay Shiomi Prince Hotel include. "
+        "Fukagawa Gatharia Shopping Mall, Tatsuminomori Kaihin Park and Susaki Stadium Monument. "
+        "\nThe nearest airport is Tokyo Haneda International Airport, 12 km from the hotel. ",
+
+  },
+  {
+    "name": "Hotel Lutetia",
+    "img": "assets/7.jpg",
+    "price": r"$300/night",
+    "location": "Paris, France",
+    "details": "Hotel Lutetia is an iconic luxury hotel situated in the Saint-Germain-des-Prés area.  "
+        "This 4-star hotel offers a 24-hour front desk. "
+        "Guest at our Hotel can enjoy a buffet breakfast. "
+        "\nAt the accommodation guests are welcome to take advantage of a sauna. "
+        "Popular points of interest near Tokyo Bay Shiomi Prince Hotel include. "
+        "Fukagawa Gatharia Shopping Mall, Tatsuminomori Kaihin Park and Susaki Stadium Monument. "
+        "\nThe nearest airport is Tokyo Haneda International Airport, 12 km from the hotel. ",
+
+  },
+  {
+    "name": "The Plaza Hotel",
+    "img": "assets/8.jpg",
+    "price": r"$150/night",
+    "location": "New York, America",
+    "details": "Pellentesque in ipsum id orci porta dapibus. "
+        "Nulla porttitor accumsan tincidunt. Donec rutrum "
+        "congue leo eget malesuada. "
+        "\n\nPraesent sapien massa, convallis a pellentesque "
+        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
+        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
+        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
+        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
+        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+  },
+  {
+    "name": "San Pietro Panoramic View",
+    "img": "assets/9.jpg",
+    "price": r"$100/night",
+    "location": "Rome, Italy",
+    "details": "Pellentesque in ipsum id orci porta dapibus. "
+        "Nulla porttitor accumsan tincidunt. Donec rutrum "
+        "congue leo eget malesuada. "
+        "\nPraesent sapien massa, convallis a pellentesque "
+        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
+        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
+        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
+        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
+        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+  },
+  {
+    "name": "Enderun Hotel Istanbul",
+    "img": "assets/10.jpg",
+    "price": r"$60/night",
+    "location": "Istanbul, Turkey",
+    "details": "Pellentesque in ipsum id orci porta dapibus. "
+        "Nulla porttitor accumsan tincidunt. Donec rutrum "
+        "congue leo eget malesuada. "
+        "\nPraesent sapien massa, convallis a pellentesque "
+        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
+        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
+        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
+        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
+        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+  },
+];
